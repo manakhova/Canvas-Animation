6c439b90541f2ac46b6d330b1bbbc8549c0d50e0
Tatiana Manakhova <tat.manakhova@gmail.com>
2022-04-02 09:55:31 +0300

    Tets

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: `./src/index.js`,
+    output: {
+        filename: `bundle.js`,
+        path: path.resolve(__dirname, `public`)
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, `public`),
+        open: false,
+        port: 1337
+        
+    },
+    module: {
+        rules: [
+          {
+            test: /\.s?css$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          { 
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+            use: ['url-loader?limit=100000'] 
+          }
+        ],
+      },
+    resolve: {
+      extensions: [`.js`, '.css', '.scss']
+    },
+    devtool: `source-map`,
+    plugins: [
+      new MiniCssExtractPlugin({filename: 'style.css'})
+    ]
+};
